2df63be945fd649d80573a21c03380d4af7f1fe8
Kateryna Fesyk <82673991+Katia-Radchenko@users.noreply.github.com>
2026-02-10 09:49:31 +0200

    Update jhomework3.js

--- jhomework3.js
+++ jhomework3.js
@@ -1,9 +1,19 @@
 
 function pow(x, y) {
+
+   if (x === 0 && y === 0) return undefined;
+
+   if (x === 0 && y < 0) return undefined;
+
+    if (y === 0) return 1;
+    
     let sum = 1
     for (let i = 0; i < y; i++) {
         sum *= x
     }
     return sum
+   
 }
-pow(2,3)
+
+
+console.log(pow(2, 3));
